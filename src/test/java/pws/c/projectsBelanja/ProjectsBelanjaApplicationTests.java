b2a137c163f689fa23bd3a800fb31d18aa47e78a
abianfikri <abronjoy46@gmail.com>
2022-10-27 22:13:11 +0700

    First Page SpringBoot

---
+++ src/test/java/pws/c/projectsBelanja/ProjectsBelanjaApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.projectsBelanja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsBelanjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
